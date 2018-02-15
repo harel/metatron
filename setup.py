67705a8fa792c955f7f453466ca0b849baefba33
Harel Malka <harel@harelmalka.com>
2018-02-15 20:43:54 +0000

    update setup to pypi.org

--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 from metatron.version import __version__
 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='metatron',
     version=__version__,
-    description='HTML Meta tag parser',
-    long_description='Metatron is a Python 3 (only) HTML meta tag parser. It has an emphasis'
-                     'on complex meta tag structures, and was created to parse OpenGraph'
-                     'and Twitter Card meta tags, including handling of arrays, although'
-                     'it can handle any meta tag scheme.',
+    description='Python 3 HTML meta tag parser, with emphasis on complex meta tag structures '
+                'with support for OpenGraph and Twitter Card tags, including array handling',
+    long_description=long_description,
     author='Harel Malka',
     author_email='harel@harelmalka.com',
     url='https://github.com/harel/metatron',
     download_url='https://github.com/harel/metatron/archive/0.1.tar.gz',
-    keywords=['html', 'meta', 'parser', 'opengraph', 'twittercard'],
+    keywords='html meta parser opengraph twittercard',
     install_requires=[
         'beautifulsoup4==4.6.0',
         'requests==2.18.4',
     ],
     license='MIT',
-    packages=['metatron', 'metatron.tests'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
